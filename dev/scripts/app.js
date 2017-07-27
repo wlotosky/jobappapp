5915b0d3d24d31564d4e3ca2e8539a004597180d
William Lotosky <wlotosky@gmail.com>
2017-07-27 11:45:42 -0400

    started form component

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -6,3 +6,34 @@ import {
 	Route
 } from 'react-router-dom';
 
+class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+
+		}
+	}
+	render() {
+		return (
+			<Router>
+
+			</Router>
+		)
+	}
+}
+
+class JobForm extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+
+		}
+	}
+	render() {
+		return (
+			<form className="newJob-form">
+				<input type="text" name="" onChange={this.handleChange} placeholder=""/>
+			</form>
+		)
+	}
+}
